91f3905f651d33619e2a8ccf8d2dd1ee04643fdb
Polina-Krivdina <krivdinapolina@mail.ru>
2024-02-21 22:35:15 -0800

    тест!

---
+++ Lesson_1/lesson_1_task_8.py
@@ -0,0 +1,43 @@
+def print_num(num):
+    print(8)
+print_num(print)
+
+def print_num(num):
+    print(8)
+print_num(print)
+
+def print_num(num):
+    print(0)
+print_num(print)
+
+def print_num(num):
+    print(0)
+print_num(print)
+
+def print_num(num):
+    print(5)
+print_num(print)
+
+def print_num(num):
+    print(5)
+print_num(print)
+
+def print_num(num):
+    print(5)
+print_num(print)
+
+def print_num(num):
+    print(3)
+print_num(print)
+
+def print_num(num):
+    print(5)
+print_num(print)
+
+def print_num(num):
+    print(3)
+print_num(print)
+
+def print_num(num):
+    print(5)
+print_num(print)
